719b8fc2fefa7215bd255dc54ef749c0e43627e9
LiuXiaoLong <75962798+xcancloud@users.noreply.github.com>
2025-05-06 18:18:42 +0800

    Fix deploy_service error

--- builds/cicd.sh
+++ builds/cicd.sh
@@ -113,7 +113,10 @@ maven_build () {
 # Deploy service module
 deploy_service() {
   echo "INFO: Deploying service module to ${host}"
-  ssh "$host" "mkdir -p ${REMOTE_APP_DIR} && cd ${REMOTE_APP_DIR} && sh shutdown-gm.sh" || {
+  ssh "$host" "mkdir -p ${REMOTE_APP_DIR} && mkdir -p ${REMOTE_APP_PLUGINS_DIR}" || {
+    echo "ERROR: Failed to creat app and plugins directory"; exit 1
+  }
+  ssh "$host" "cd ${REMOTE_APP_DIR} && sh shutdown-tester.sh" || {
     echo "WARN: Failed to stop service, proceeding anyway"
   }
   ssh "$host" "cd ${REMOTE_APP_DIR} && find . -mindepth 1 -path './${REMOTE_APP_STATIC_DIR_NAME}' -prune -o -exec rm -rf {} +" || {
