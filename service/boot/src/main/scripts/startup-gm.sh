1094fd8f5fa63f4b602b3f489d9b78d0f363dbda
LiuXiaoLong <75962798+xcancloud@users.noreply.github.com>
2025-05-06 18:50:25 +0800

    Fix to init JAVA_HOME

--- service/boot/src/main/scripts/startup-gm.sh
+++ service/boot/src/main/scripts/startup-gm.sh
@@ -12,7 +12,7 @@ CURRENT_HOME=`dirname "$0"`
 echo "App Home: $GM_HOME"
 
 # Init java environment
-chmod +x init-jdk.sh && sh init-jdk.sh
+chmod +x init-jdk.sh && eval "$(./init-jdk.sh)"
 echo "Java Home: ${JAVA_HOME}"
 
 # Check that target jar exists
