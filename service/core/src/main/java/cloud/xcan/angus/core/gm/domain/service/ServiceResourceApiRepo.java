9030b135d27cf4873e72984ad1d709ea99e3bbc2
LiuXiaoLong <75962798+xcancloud@users.noreply.github.com>
2025-05-29 20:46:34 +0800

    Fix ServiceResourceApiRepo code

--- service/core/src/main/java/cloud/xcan/angus/core/gm/domain/service/ServiceResourceApiRepo.java
+++ service/core/src/main/java/cloud/xcan/angus/core/gm/domain/service/ServiceResourceApiRepo.java
@@ -9,13 +9,13 @@ public interface ServiceResourceApiRepo extends BaseRepository<ServiceResourceAp
 
   @Query(value =
       "SELECT DISTINCT resource_name resourceName, resource_description resourceDesc, id apiId, "
-          + " name apiName, code apiCode, enabled apiEnabled, description apiDescription "
+          + " name apiName, operation_id apiCode, enabled apiEnabled, description apiDescription "
           + " FROM api WHERE service_code = ?1", nativeQuery = true)
   List<ServiceResourceApi> findServiceResourceApiList(String serviceCode);
 
   @Query(value =
       "SELECT DISTINCT resource_name resourceName, resource_description resourceDesc, id apiId, "
-          + " name apiName, code apiCode, enabled apiEnabled, description apiDescription "
+          + " name apiName, operation_id apiCode, enabled apiEnabled, description apiDescription "
           + " FROM api WHERE service_code = ?1 and resource_name = ?2 ", nativeQuery = true)
   List<ServiceResourceApi> findServiceResourceApiList(String serviceCode, String resourceName);
 
